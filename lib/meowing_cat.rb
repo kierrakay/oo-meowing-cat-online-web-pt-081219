5161620b989b8a7f8ef545183dfda049b883c2d4
Kierra M <k.merriman32@hotmail.com>
2019-08-31 14:43:13 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,34 +1,14 @@
 class Cat 
- 
-  def maru=(cat_name)
-    @cat = cat_name
   
-  
-  
-describe "Cat" do
-  let(:maru) {Cat.new}  # Look up what let does in RSpec
-  # https://www.relishapp.com/rspec/rspec-core/v/2-6/docs/helper-methods/let-and-let
+attr_accessor :name 
+end
 
-  it 'instantiates a new cat' do
-    expect(maru).to be_a(Cat)
-  end
+maru = Cat.new
+maru.name = "Maru"
+end
 
-  it 'receives attr_accessor' do
-    expect(Cat).to receive(:attr_accessor)
-    load('meowing_cat.rb')
-  end
-
-  it 'has a name' do
-    maru.name = "Maru"
-    expect(maru.name).to eq("Maru")
-  end
-
-   it "is able to meow" do
-
-    # STDOUT is a constant that ruby defines for you
-    # that represents 'output' so that you can test
-    # against puts and Input/Output (IO) methods
-    expect(STDOUT).to receive(:puts).with("meow!")
-    maru.meow
-  end
+def meow
+  @cat
+  puts "meow!"
+end
 end
